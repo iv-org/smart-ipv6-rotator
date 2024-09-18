8e2587e1da9b049e65323f17e82777680f029fac
Ward <wardpearce@pm.me>
2024-09-19 09:00:53 +1200

    Python logging support (#29)
    * Add logging support
    
    * Support lowercase log level choices
    
    * Update README

--- smart_ipv6_rotator/const.py
+++ smart_ipv6_rotator/const.py
@@ -1,3 +1,5 @@
+import logging
+
 from pyroute2 import IPDB, IPRoute
 
 ICANHAZIP_IPV6_ADDRESS = "2606:4700::6812:7261"
@@ -5,6 +7,9 @@ ICANHAZIP_IPV6_ADDRESS = "2606:4700::6812:7261"
 JSON_CONFIG_FILE = "/tmp/smart-ipv6-rotator.json"
 
 LEGACY_CONFIG_FILE = "/tmp/smart-ipv6-rotator.py"
+
+LOGGER = logging.getLogger(__name__)
+LOG_LEVELS_NAMES = list(logging._nameToLevel.keys())
 
 IP = IPDB()
 IPROUTE = IPRoute()
